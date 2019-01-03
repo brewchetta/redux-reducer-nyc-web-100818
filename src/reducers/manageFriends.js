a26eb525b2163cf4294cfca063ae517dfc324da1
brewchetta <chett.tiller@gmail.com>
2019-01-03 08:47:28 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND' :
+      return { friends: [...state.friends, action.friend] }
+    case 'REMOVE_FRIEND' :
+      const removedFriend = state.friends.find(friend => friend.id === action.id)
+      const newFriends = [...state.friends]
+      newFriends.splice(state.friends.indexOf(removedFriend), 1)
+      return { friends: newFriends }
+    default:
+      return state
+  }
+
 }
